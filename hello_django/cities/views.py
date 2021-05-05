6e361ce6ce800ef57513865856eb1f4c56e5bf6b
EUGINELETHAL <ochungeugine@gmail.com>
2021-05-05 10:00:41 +0300

    instatied postgres

--- hello_django/cities/views.py
+++ hello_django/cities/views.py
@@ -6,16 +6,26 @@ from django.contrib.postgres.search import SearchVector,TrigramSimilarity
 from .models import City
 
 
-class HomePageView(TemplateView):
+class HomePageView(ListView):
     template_name = 'home.html'
+    model = City.objects.all()
+    template_name = 'search_results.html'
+    
+    def get_queryset(self): 
+     	object_list= City.objects.all()
+     
+        return object_list
 
 class SearchResultsView(ListView):
     model = City
     template_name = 'search_results.html'
     
+    
     def get_queryset(self): 
         query = self.request.GET.get('q')
         object_list1= City.objects.annotate(similarity=TrigramSimilarity('name', query),).filter(similarity__gt=0.1).order_by('-similarity')
         object_list2=City.objects.annotate(similarity=TrigramSimilarity('state', query),).filter(similarity__gt=0.1).order_by('-similarity')
+        object_list= object_list1|object_list2   # merge querysets
+        return object_list(similarity=TrigramSimilarity('state', query),).filter(similarity__gt=0.1).order_by('-similarity')
         object_list= object_list1|object_list2   # merge querysets
         return object_list
