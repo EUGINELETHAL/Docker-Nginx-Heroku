ba62c576d1753efb913b1ab7a6731217793ceedb
EUGINELETHAL <ochungeugine@gmail.com>
2021-05-05 10:19:55 +0300

    instatied postgres

--- hello_django/cities/views.py
+++ hello_django/cities/views.py
@@ -24,5 +24,5 @@ class SearchResultsView(ListView):
         object_list1= City.objects.annotate(similarity=TrigramSimilarity('name', query),).filter(similarity__gt=0.1).order_by('-similarity')
         object_list2=City.objects.annotate(similarity=TrigramSimilarity('state', query),).filter(similarity__gt=0.1).order_by('-similarity')
         object_list= object_list1|object_list2   # merge querysets
-        return object_list(similarity=TrigramSimilarity('state', query),).filter(similarity__gt=0.1).order_by('-similarity')
+        return object_list
         
